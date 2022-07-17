7bbe7429804ab6b3bab7e013b88e2f2e4eb1b59e
itsaliataie <aataie.b1t@gmail.com>
2022-07-17 16:30:03 -0400

    User Profile
    Added the functionality to show user profile when clicked on user

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import { createRenderer } from "react-dom/test-utils";
 import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
@@ -9,6 +10,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
   const initialState = {
     users: [],
+    user: {},
+    repos:[],
     loading: false
   }
 
@@ -40,6 +43,60 @@ export const GithubProvider = ({children}) => {
     })
   }
 
+  // Get single user
+  const getUser = async (login) => {
+    setLoading()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`)
+
+    // Added a Github Token, but I had to keep changing it because it woul fail every new day.
+    // Adding the token would look something like this:
+    // , {
+    //   headers: {
+    //     authorization: `token ${GITHUB_TOKEN}`
+    //   }
+    // }
+
+    if(response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+  
+      dispatch({
+        type: 'GET_USER',
+        payload: data
+      })
+    }
+
+  }
+
+  // Get User Repos
+  const getUserRepos = async (login) => {
+    setLoading()
+
+    const params = new URLSearchParams({
+      sort: 'created',
+      per_page: 10
+    })
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)
+
+    // Added a Github Token, but I had to keep changing it because it woul fail every new day.
+    // Adding the token would look something like this:
+    // , {
+    //   headers: {
+    //     authorization: `token ${GITHUB_TOKEN}`
+    //   }
+    // }
+
+    const data = await response.json()
+
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data
+    })
+  }
+
   // Clear users from state
   const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
 
@@ -48,8 +105,12 @@ export const GithubProvider = ({children}) => {
   return <GithubContext.Provider value={{
     users: state.users,
     loading: state.loading,
+    user: state.user,
+    repos: state.repos,
     searchUsers,
-    clearUsers
+    clearUsers,
+    getUser,
+    getUserRepos
   }}>
     {children}
   </GithubContext.Provider>
